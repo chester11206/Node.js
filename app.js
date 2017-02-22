e491f77d0d4bef0006a386e4277489a1ba3f04ec
chester11206 <b04705039@ntu.edu.tw>
2017-02-22 20:21:54 +0800

    17/2/22

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-var a = 3
-var b = 5
-var c = a + b
-console.log("Hello World")
+var http = require("http");
+
+function onRequest(req, res){
+	console.log("Request received.");
+	res.writeHead(200, {"Content-Type": "text/plain"});
+	res.end("Hello World!");
+}
+
+http.createServer(onRequest).listen(1234);
+
+console.log("Server has started to listen at port: 1234.");
